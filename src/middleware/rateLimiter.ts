93d42eac79c714487d772e13d4339c05477630a4
RaghhavDTurki <raghhavdturki@gmail.com>
2021-08-09 09:34:46 +0530

    Add Rate Limiter

--- src/middleware/rateLimiter.ts
+++ src/middleware/rateLimiter.ts
@@ -6,7 +6,7 @@ import redis from 'redis'
 export const limiter = RateLimit({
     store: new RedisStore({
         client: redis.createClient({
-            host: <string>process.env.REDIS_HOST,
+            host: "redis-18153.c212.ap-south-1-1.ec2.cloud.redislabs.com",
             port: parseInt(<string>process.env.REDIS_PORT, 10),
             password: <string>process.env.REDIS_PASSWORD
         })
