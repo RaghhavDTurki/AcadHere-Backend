2b72adda333c58a2a75b34f9eeadb37ee14f5366
RaghhavDTurki <raghhavdturki@gmail.com>
2021-11-23 11:37:05 +0530

    Removing additional fields on Mongoose Connection

--- src/database/NoticeBoard_Connection.ts
+++ src/database/NoticeBoard_Connection.ts
@@ -3,13 +3,7 @@ import mongoose from "mongoose";
 export const connectNoticeBoardDB = async () => {
     try {
         const connection = await mongoose.connect(
-            <string>process.env.MONGO_URI_NOTICE_BOARD,
-            {
-                useNewUrlParser: true,
-                useFindAndModify: true,
-                useUnifiedTopology: true,
-                useCreateIndex: true,
-            })
+            <string>process.env.MONGO_URI_NOTICE_BOARD)
 
             console.log(`MongoDB connected: ${connection.connection.host}`)
     } catch (error) {
